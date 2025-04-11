30b92176841a2cd9c088f83163c0ceee0914497b
Anuolu-2020 <Anuolu1645@gmail.com>
2025-04-11 01:31:48 +0100

    feat: moved validation annotations to interface

--- src/main/java/com/example/sosbackend/controller/interfaces/EmergencyServiceApi.java
+++ src/main/java/com/example/sosbackend/controller/interfaces/EmergencyServiceApi.java
@@ -15,18 +15,22 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.Valid;
+
 @Tag(name = "Emergency Services", description = "Apis for emergency services routes")
 public interface EmergencyServiceApi {
 
   @Operation(summary = "Create new emergency services", description = "create new emergency services with their details")
   public ResponseEntity<ApiResponse<List<EmergencyServiceResponseDTO>>> createEmergencyService(
-      List<CreateEmergencyServiceRequest> requests, HttpServletRequest request);
+      @Valid List<@Valid CreateEmergencyServiceRequest> requests, HttpServletRequest request);
 
   @Operation(summary = "Get emergency services within a coordinate", description = "Fetch emergency services with a coordinate within a radius")
   public ResponseEntity<ApiResponse<List<EmergencyServiceResponseDTO>>> findNearbyEmergencyServices(
-      double longitude,
-      double latitude,
-      @Parameter(description = "Search radius in meters", required = true)
-      double radius,
+      @DecimalMin(value = "-180.0", message = "longitude must be greater than or equal to -180.0") @DecimalMax(value = "180.0", message = "longitude must be less than or equal to 180.0") double longitude,
+      @DecimalMin(value = "-90.0", message = "latitude must be greater than or equal -90.0") @DecimalMax(value = "90.0", message = "latitude must be less than or equal to 90.0") double latitude,
+      @Parameter(description = "Search radius in meters", required = true) @Positive(message = "radius must be positive") double radius,
       HttpServletRequest request) throws ResourceNotFoundException;
 }
