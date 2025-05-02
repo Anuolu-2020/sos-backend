1205a457ffa89b4a7f0b1516fad09324c898a6b7
Anuolu-2020 <Anuolu1645@gmail.com>
2025-05-02 23:37:20 +0100

    feat: incident report response dto implemented

---
+++ src/main/java/com/example/sosbackend/dto/IncidentReportResponseDTO.java
@@ -0,0 +1,50 @@
+
+package com.example.sosbackend.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.sosbackend.model.IncidentReportsModel;
+import com.example.sosbackend.model.IncidentReportsPicturesModel;
+import com.example.sosbackend.model.IncidentReportsVideosModel;
+
+import lombok.Data;
+
+@Data
+public class IncidentReportResponseDTO {
+  private Long id;
+  private String typeOfIncident;
+  private String description;
+  private List<IncidentReportsPicturesModel> pictures;
+  private List<IncidentReportsVideosModel> videos;
+  private double longitude;
+  private double latitude;
+
+  public static IncidentReportResponseDTO from(IncidentReportsModel model) {
+
+    IncidentReportResponseDTO dto = new IncidentReportResponseDTO();
+
+    dto.setId(model.getId());
+
+    dto.setTypeOfIncident(model.getTypeOfIncident());
+
+    dto.setDescription(model.getDescription());
+
+    dto.setPictures(model.getPictures());
+
+    dto.setVideos(model.getVideos());
+
+    if (model.getCoordinates() != null) {
+      dto.setLongitude(model.getCoordinates().getPosition().getLon());
+      dto.setLatitude(model.getCoordinates().getPosition().getLat());
+    }
+    return dto;
+  }
+
+  public static List<IncidentReportResponseDTO> from(List<IncidentReportsModel> models) {
+    return models.stream()
+        .map(IncidentReportResponseDTO::from)
+        .collect(Collectors.toList());
+  }
+
+}
